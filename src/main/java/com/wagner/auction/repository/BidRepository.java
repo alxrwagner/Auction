99e24dd71a178efa398766dbdfe10d36ffac3998
alxrw <alxr.wagner@gmail.com>
2023-05-05 11:43:55 +0700

    Добавлен README.md

--- src/main/java/com/wagner/auction/repository/BidRepository.java
+++ src/main/java/com/wagner/auction/repository/BidRepository.java
@@ -16,7 +16,7 @@ public interface BidRepository extends JpaRepository<Bid, Long> {
 
     @Query(value = "SELECT * FROM bid WHERE lot_id = ?1 ORDER BY bid_date LIMIT 1", nativeQuery = true)
     Bid findFirstBid(Long id);
-
+      
     @Query(value = "select bidder_name as bidderName, bid_date as bidDate from (select * from bid where bidder_name = (\n" +
             "select bidder_name from bid where lot_id = ?1 group by bidder_name order by count(*) desc limit 1)) as \"b*\" order by bid_date desc limit 1", nativeQuery = true)
     FrequentView findFrequent(Long id);
